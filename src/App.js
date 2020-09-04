63c3b5616deca6c6b331465251a56ec22d25b459
- <dhcjf1004@naver.com>
2020-09-04 14:13:33 +0000

    기존 repo 파일 이동

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Counter from './Counter';
+// import Say from './Say'
+// import logo from './logo.svg';
 import './App.css';
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <img
+                    src="https://img.hankyung.com/photo/201908/BF.20397825.1.jpg"
+                    className="App-logo"
+                    alt="logo"
+                />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+				
+				<Counter/>
+				{React.version}
+				
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+            name: undefined,
+        };
+    }
+
+    render() {
+        const { number, name } = this.state;
+
+        return (
+            <div>
+                {number % 2 === 0 ? <h1>{number}</h1> : <h2>{number}</h2>}
+
+                <button
+                    onClick={() => {
+                        this.setState(() => {
+                            if (name === undefined) {
+                                this.setState({ name: '하면 안돼요.' });
+                            } else {
+                                this.setState({ name: undefined });
+                            }
+							console.log(name)
+                            return {
+                                number: number + 1,
+                                name,
+                            };
+                        });
+                    }}
+                >
+                    +1
+                </button>
+                {name === undefined ? <h1>{String(name)}</h1> : <h1>{name}</h1>}
+            </div>
+        );
+    }
+}
+export default Counter;
