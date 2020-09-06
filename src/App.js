21c014f6914de57f609a4c7ca309afe832317e4b
- <dhcjf1004@naver.com>
2020-09-06 10:32:19 +0000

    study

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import Counter from './Counter';
-import Say from './Hooks/Say';
-import EventPractice from './Event/EventPractice';
-import ValidityState from './Dom_name/ValidationSample'
+//import Counter from './Counter';
+//import Say from './Hooks/Say';
+//import EventPractice from './Event/EventPractice';
+//import ValidityState from './Dom_name/ValidationSample';
+//import IterationSample from './Repeat_component/IterationSample';
+import LifeCycleSample from './LifeCycle/LifeCycleSample';
+import ErrorBoundary from './LifeCycle/ErrorBoundary'
+
 // import ScrollBox from './Dom_name/ScrollBox'
 // import logo from './logo.svg';
 import './App.css';
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 1677215).toString(16);
+}
 class App extends Component {
+    state = {
+        color: '#000000',
+    };
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor(),
+        });
+    };
     render() {
         return (
             <div className="App">
@@ -15,29 +32,29 @@ class App extends Component {
                     className="App-logo"
                     alt="logo"
                 />
+                <button onClick={this.handleClick}>랜덤 색상</button>
+				
+				<ErrorBoundary>
+				<LifeCycleSample  color = {this.state.color}/>
+				</ErrorBoundary>
+                
+				
+				{React.version}
+				
+            </div>
 
-				
-				<Counter/>
-				
-				<Say/>
-				
-				<EventPractice/>
-				
-				--------------------------
-				
-				<ValidityState/>
-				
-				 </div>
-				/*<ScrollBox ref = {(ref) => {this.ScrollBox = ref}}/> -->
+            /*<Counter />
+                <Say />
+                <EventPractice />
+                --------------------------
+                <ValidityState />
+                <IterationSample />*/
+
+            /*<ScrollBox ref = {(ref) => {this.ScrollBox = ref}}/> -->
 				<button onclick = {()=>{this.scrollBox.ScrollToBottom()}}>맨 밑으로</button> */
-				
-				/* 스크롤바 문제 못풀겠다. 왜 작동 안되는지 모르겠음*/
-					
-					
-				
-           
+
+            /* 스크롤바 문제 못풀겠다. 왜 작동 안되는지 모르겠음*/
         );
     }
 }
 export default App;
-
