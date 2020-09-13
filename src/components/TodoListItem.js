d0158564098186726118a2485cad45a71fb06575
vernonKwon <kwondroid@daum.net>
2020-09-13 08:22:51 +0000

    classnames 사용 방법 주석

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -23,3 +23,21 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
 };
 
 export default TodoListItem;
+
+
+/**
+classNames('foo', 'bar'); // => 'foo bar'
+classNames('foo', { bar: true }); // => 'foo bar'
+classNames({ 'foo-bar': true }); // => 'foo-bar'
+classNames({ 'foo-bar': false }); // => ''
+classNames({ foo: true }, { bar: true }); // => 'foo bar'
+classNames({ foo: true, bar: true }); // => 'foo bar'
+classNames(['foo', 'bar']); // => 'foo bar'
+
+// 동시에 여러개의 타입으로 받아올 수 도 있습니다.
+classNames('foo', { bar: true, duck: false }, 'baz', { quux: true }); // => 'foo bar baz quux'
+
+// false, null, 0, undefined 는 무시됩니다.
+classNames(null, false, 'bar', undefined, 0, 1, { baz: null }, ''); // => 'bar 1'
+
+*/
