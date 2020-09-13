79ebb4e7d678ee8c918752e6b7c3ca7482776a8f
vernonKwon <kwondroid@daum.net>
2020-09-13 08:16:39 +0000

    일정 관리 앱 애플리케이션 완성

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [todos, setTodos] = useState([
+        { id: 1, text: '전역하고싶다', checked: true },
+        { id: 2, text: '마스터 치킨 사주세요.', checked: false },
+        { id: 3, text: '뿌링클 사랑해요', checked: true },
+    ]);
 
+    const nextId = useRef(todos.length + 1);
+
+    const onInsert = useCallback(
+        (text) => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked: false,
+            };
+            setTodos(todos.concat(todo));
+            nextId.current += 1;
+        },
+        [todos]
+    );
+
+    const onRemove = useCallback(
+        (id) => {
+            setTodos(todos.filter((todo) => todo.id !== id));
+        },
+        [todos]
+    );
+
+    const onToggle = useCallback((id) => {
+        setTodos(
+            todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo))
+        );
+    }, [todos]);
+
+    return (
+        <div>
+            <TodoTemplate>
+                <TodoInsert onInsert={onInsert} />
+                <TodoList todos={todos} onRemove={onRemove} onToggle = {onToggle} />
+            </TodoTemplate>
+        </div>
+    );
+};
 export default App;
