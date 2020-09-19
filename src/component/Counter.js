81d3c07e3acd2269421f7ee804ef370d54396647
vernonKwon <kwondroid@daum.net>
2020-09-19 04:33:31 +0000

    리덕스 - 컴포넌트 연결

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,16 +1,68 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import CounterButton from './CounterButton';
 import CounterNumber from './CounterNumber';
 
-const Counter = () => {
-    const [number, setNumber] = useState(3);
-	console.log(JSON.stringify(number))
-    return (
-        <div>
-            <CounterNumber number={number} />
-            <CounterButton number={number} />
-        </div>
-    );
+import { connect } from 'react-redux';
+
+import * as action from '../action';
+
+class Counter extends Component {
+    //const [number, setNumber] = useState(3);
+    constructor(props) {
+        super(props);
+
+		this.setRandomColor = this.setRandomColor.bind(this)
+    }
+	
+	setRandomColor() {
+        const color = [
+            Math.floor(Math.random() * 55 + 200),
+            Math.floor(Math.random() * 55 + 200),
+            Math.floor(Math.random() * 55 + 200),
+        ];
+		
+		this.props.handleSetColor(color)
+    }
+
+    render() {
+		
+		const color = this.props.color;
+		
+		const style = {
+			background : `rgb(${color[0]},${color[1]},${color[2]})`
+		}
+        return (
+            <div style = {style}>
+                <CounterNumber number={this.props.number} />
+                <CounterButton
+                    plus={this.props.handleIncrement}
+                    minus={this.props.handleDecrement}
+					background = {this.setRandomColor}
+                />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        number: state.counter.number,
+        color: state.ui.color,
+    };
 };
 
-export default Counter;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleIncrement: () => {
+            dispatch(action.increment());
+        },
+        handleDecrement: () => {
+            dispatch(action.decrement());
+        },
+        handleSetColor: (color) => {
+            dispatch(action.setColor(color));
+        },
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/component/CounterButton.js
+++ src/component/CounterButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const CounterButton = () => {
+const CounterButton = ({ plus, minus, background }) => {
     return (
         <div>
-            <button >증가 (+)</button>
-            <button >감소 (-)</button>
+            <button onClick={plus}>증가 (+)</button>
+            <button onClick={minus}>감소 (-)</button>
+            <button onClick={background}>배경 색깔</button>
         </div>
     );
 };
 
-export default CounterButton
+export default CounterButton;
